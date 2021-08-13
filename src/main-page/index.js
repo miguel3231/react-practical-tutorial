393d55f0bf03bd2d0d7ae93c5ab02f3cb7d43901
Miguel Beltrán <miguel.socialmedia@outlook.com>
2021-08-12 22:51:21 -0700

    globomantics page

---
+++ src/main-page/index.js
@@ -0,0 +1,42 @@
+import {useEffect, useState, useMemo} from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './main-page.css';
+import Header from "./header";
+import FeaturedHouse from "./featured-house";
+function App() {
+  const [allHouses, setAllHouses] = useState([]);
+
+  useEffect(() => {
+    const fetchHouses = async () =>{
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, []);
+
+  const featuredHouse = useMemo(()=>{
+    if(allHouses.length){
+      const randomIndex= Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex];
+    }
+  }, [allHouses]);
+
+  return (
+    <Router>
+       <div className="container">
+        <Header 
+          subtitle="Providing houses all over the world"
+         />
+         <Switch>
+           <Route path="/">
+              <FeaturedHouse house={featuredHouse}></FeaturedHouse>
+           </Route>
+         </Switch>
+       </div>
+    </Router>
+    
+  );
+}
+
+export default App;
